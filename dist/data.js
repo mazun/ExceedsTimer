417bbad9880bd950544c028ad5afe4a2975e50b7
y_mazun <y.mazun@gmail.com>
2017-03-02 01:00:59 +0900

    最低限実装

---
+++ dist/data.js
@@ -0,0 +1,23 @@
+data = {
+    title: "普段エクシ",
+    actions: [
+        {
+            minute: 15,
+            name: "エクシ開始",
+            note: "頑張ろー"
+        },
+        {
+            minute: 20,
+            name: "インスピ",
+            note: "殲滅"
+        },
+        {
+            minute: 30,
+            name: "インスピ",
+        },
+        {
+            minute: 45,
+            name: "インスピ"
+        }
+    ]
+}
---
+++ src/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const CurrentAction = props => {
+    const { action } = props;
+    const text = action ? action.minute + "分 " + action.name + (action.note ? " (" + action.note + ")" : "") : "";
+    return (
+        <div>現: {text}</div>
+    );
+};
+
+const NextAction = props => {
+    const { action, date } = props;
+    const text = (() => {
+        if (action) {
+            const { name, minute } = action;
+            const m = date.getMinutes();
+            const s = date.getSeconds();
+            const restMinute = minute - m - (s == 0 ? 0 : 1);
+            const restSeconds = s == 0 ? 0 : 60 - s;
+            return minute + "分 " + name + " (" + restMinute + ":" + ("0" + restSeconds).slice(-2) + ")"
+        } else {
+            return "";
+        }
+    })();
+
+    return (
+        <div className="small">次: {text}</div>
+    );
+}
+
+const Exceeds = props => {
+    const { data, date } = props;
+
+    const extract = (data, date) => {
+        const minute = date.getMinutes();
+        const actions = data.actions;
+
+        var i = 0;
+        while (i < actions.length && actions[i].minute <= minute) i++;
+
+        const current = 0 <= i - 1 && i - 1 < actions.length ? actions[i - 1] : undefined;
+        const next = 0 <= i && i < actions.length ? actions[i] : undefined;
+
+        return { current, next };
+    };
+
+    const { current, next } = extract(data, date);
+
+    return (
+        <div>
+            <h3>{data.title}</h3>
+            <p>現在時刻: {date.toLocaleTimeString()}</p>
+            <CurrentAction action={current} />
+            <NextAction action={next} date={date} />
+        </div>
+    );
+};
+
+setInterval(() => {
+    const date = new Date();
+    render(<Exceeds data={data} date={date} />, document.getElementById('app'));
+}, 100);
